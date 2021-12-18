1c15a77bb9f23675a02710f2a764db042712d6c4
Kamil Leszczuk <kamituel@gmail.com>
2021-12-18 08:14:39 +0100

    R-pentomino and puffer train boards.

--- src/game_of_life/boards.cljs
+++ src/game_of_life/boards.cljs
@@ -10,80 +10,131 @@
     board-type))
 
 
+(defn all-dead-board [height width]
+
+  (let [board (make-array (* height width))]
+    (dotimes [pos (* width height)]
+      (aset board pos game/dead))
+    board))
+
+
 (defn set-cell [board width row-index col-index status]
 
   (aset board (+ (* row-index width) col-index) status))
 
 
 (def pulsar-template
-  ["..XXX...XXX.."
+  ["..OOO...OOO.."
    "............."
-   "X....X.X....X"
-   "X....X.X....X"
-   "X....X.X....X"
-   "..XXX...XXX.."
+   "O....O.O....O"
+   "O....O.O....O"
+   "O....O.O....O"
+   "..OOO...OOO.."
    "............."
-   "..XXX...XXX.."
-   "X....X.X....X"
-   "X....X.X....X"
-   "X....X.X....X"
+   "..OOO...OOO.."
+   "O....O.O....O"
+   "O....O.O....O"
+   "O....O.O....O"
    "............."
-   "..XXX...XXX.."])
+   "..OOO...OOO.."])
 
 
 (def blinker-template
   
-  ["XXX"])
+  ["OOO"])
 
 
 (def toad-template
   
-  [".XXX"
-   "XXX."])
+  [".OOO"
+   "OOO."])
 
 
 (def beacon-template
   
-  ["XX..."
-   "XX..."
-   "..XX"
-   "..XX"])
+  ["OO..."
+   "OO..."
+   "..OO"
+   "..OO"])
 
 
 (def glider-template
   
-  ["..X"
-   "X.X"
-   ".XX"])
+  ["..O"
+   "O.O"
+   ".OO"])
 
 
 (def r-pentomino-template
   
-  [".XX"
-   "XX."
-   ".X."])
+  [".OO"
+   "OO."
+   ".O."])
 
 
 (def gosper-glider-gun
   
   [
-   "........................X..........."
-   "......................X.X..........."
-   "............XX......XX............XX"
-   "...........X...X....XX............XX"
-   "XX........X.....X...XX.............."
-   "XX........X...X.XX....X.X..........."
-   "..........X.....X.......X..........."
-   "...........X...X...................."
-   "............XX......................"])
+   "........................O..........."
+   "......................O.O..........."
+   "............OO......OO............OO"
+   "...........O...O....OO............OO"
+   "OO........O.....O...OO.............."
+   "OO........O...O.OO....O.O..........."
+   "..........O.....O.......O..........."
+   "...........O...O...................."
+   "............OO......................"])
 
 
 (def lightweight-space-ship-template
 
-  [".X..X"
-   "X...."
-   "X...X"
-   "XXXX."])
+  [".O..O"
+   "O...."
+   "O...O"
+   "OOOO."])
+
+
+;; By Bill Gosper, 1971
+(def puffer-train-1-template
+
+  [".OOO......O.....O......OOO."
+   "O..O.....OOO...OOO.....O..O"
+   "...O....OO.O...O.OO....O..."
+   "...O...................O..."
+   "...O..O.............O..O..."
+   "...O..OO...........OO..O..."
+   "..O...OO...........OO...O.."])
+
+
+;; By Richard Schank, 2014
+(def puffer-train-2-template
+  
+  ["...O.......O..."
+   "..OOO.....OOO.."
+   ".OO..O...O..OO."
+   "...OOO...OOO..."
+   "..............."
+   "....O.....O...."
+   "..O..O...O..O.."
+   "O.....O.O.....O"
+   "OO....O.O....OO"
+   "......O.O......"
+   "...O.O...O.O..."
+   "....O.....O...."])
+
+
+(defn rotate-template [template]
+
+  (loop [n                0
+         rotated-template []]
+    
+    (if (= n (count (first template)))
+
+      (map str/join rotated-template)
+
+      (recur (inc n)
+             (conj rotated-template
+                   (map #(nth % n) template))))))
 
 
 (defn draw-template [board width row-index col-index template]
@@ -91,7 +142,7 @@
   (doseq [[row-offset row] (utils/add-index template)]
     (doseq [[col-offset status] (-> row
                                     (str/split #"")
-                                    (->> (map #(if (= "X" %) game/alive game/dead)))
+                                    (->> (map #(if (= "O" %) game/alive game/dead)))
                                     utils/add-index)]
       (set-cell board width (+ row-index row-offset) (+ col-index col-offset) status))))
 
@@ -122,3 +173,35 @@
       (draw-template width 60 220 lightweight-space-ship-template)
       (draw-template width 40 20 gosper-glider-gun)
       #_(draw-template width 20 90 r-pentomino-template))))
+
+
+(defmethod generate-board :r-pentomino [_ height width]
+
+  (doto (all-dead-board height width)
+    (draw-template width (int (/ height 2)) (int (/ width 2)) r-pentomino-template)))
+
+
+(defmethod generate-board :puffer-train-1 [_ height width]
+  
+  (let [template (->> puffer-train-1-template
+                      reverse
+                      rotate-template)]
+    (doto (all-dead-board height width)
+      (draw-template width
+                     (int (- (/ height 2)
+                             (/ (count template) 2)))
+                     2
+                     template))))
+
+
+(defmethod generate-board :puffer-train-2 [_ height width]
+
+  (let [template (->> puffer-train-2-template
+                      reverse
+                      rotate-template)]
+    (doto (all-dead-board height width)
+      (draw-template width
+                     (int (- (/ height 2)
+                             (/ (count template) 2)))
+                     2
+                     template))))
